715669ce8d9130a9314de2ce471e317a91b977bc
Abdellah Aarab <105945475+abdellahaarab@users.noreply.github.com>
2026-03-19 01:22:52 +0100

    Enhance navbar with mobile menu toggle and mode button
    Added functionality to toggle mobile menu and update mode button text.

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,3 +2,14 @@
 document.getElementById("modeBtn").onclick = () => {
   document.body.classList.toggle("dark");
 };
+
+  // Toggle menu mobile
+  document.getElementById("menuBtn").onclick = () => {
+    document.getElementById("navLinks").classList.toggle("show");
+  };
+  
+  // Dark / Light mode
+  document.getElementById("modeBtn").onclick = () => {
+    document.body.classList.toggle("light");
+    modeBtn.textContent = document.body.classList.contains("light") ? "🌙" : "☀️";
+  };
